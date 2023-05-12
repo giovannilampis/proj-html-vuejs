b6d233e9164b9df666081acddfaf6c4287cb8bd5
giovannilampis <giolampis@hotmail.com>
2023-05-12 18:28:35 +0200

    add content to SummaryComp.vue

--- src/components/SummaryComp.vue
+++ src/components/SummaryComp.vue
@@ -13,8 +13,20 @@ export default {
 
 <template>
     <div class="row">
-        <div class="col-3" v-for="">
-
+        <div class="col-3" v-for="product,index of subSummary" :key="index">
+            <div class="d-flex">
+                <div class="flex-1" style="flex: 1">
+                    {{ product.title }}
+                    Stelline
+                    <span v-if="product.sale_price != null">
+                        $<del>{{ product.price }}</del> ${{ product.sale_price }}
+                    </span>
+                    <span v-else>
+                        ${{ product.price }}
+                    </span>
+                </div>
+                <img class="w-5" :src="'/images/' + product.img" alt="">
+            </div>
         </div>
     </div>
 </template>
