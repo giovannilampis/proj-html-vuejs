c6fa7a041696d0e2d12f887330e77d16ec8fd4ed
giovannilampis <giolampis@hotmail.com>
2023-05-12 18:05:41 +0200

    add subSummary.js to store

---
+++ src/store/subSummary.js
@@ -0,0 +1,32 @@
+
+import { reactive } from "vue"
+export const products = reactive({
+   feature: [
+    {
+        img: '',
+        title: '',
+        price: 2,
+        sale_price: 1
+    }
+   ],
+   "on sale": [
+    {
+        img: '',
+        title: '',
+        price: 2,
+        sale_price: 1
+    }, {
+        img: '',
+        title: '',
+        price: 2,
+        sale_price: 1
+    }
+   ],
+   "top rated": [
+
+   ],
+   "latest reviews": [
+
+   ]
+})
+
