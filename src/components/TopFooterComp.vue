f8a21bac32ff1b4610e9b97bd122836e9e16f4dd
giovannilampis <giolampis@hotmail.com>
2023-05-12 18:18:28 +0200

    add footer.js to store import data to TopFooter.

--- src/components/TopFooterComp.vue
+++ src/components/TopFooterComp.vue
@@ -1,14 +1,42 @@
 <script>
-
+import { recentPost, tags, topRatedProducts } from '../store/footer'
 export default {
-    name: 'TopFooterComp'
+    name: 'TopFooterComp',
+    data() {
+        return {
+            recentPost, tags, topRatedProducts 
+        }
+    }
 }
 
 </script>
 
 <template>
-    <div>
-        
+    <div class="row">
+        <div class="col-3">
+            <p>AVADA</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, exercitationem.</p>
+            <div>
+                <a href="http://facebook" target="_blank" rel="noopener noreferrer">
+
+                </a>
+                <a href="http://twitter" target="_blank" rel="noopener noreferrer">
+                    
+                </a>
+                <a href="http://instagram" target="_blank" rel="noopener noreferrer">
+                    
+                </a>
+                <a href="http://youtube" target="_blank" rel="noopener noreferrer">
+                    
+                </a>
+            </div>
+        </div>
+        <div v-for="product,index of topRatedProducts" :key="index">
+        </div>
+        <div v-for="post,index of recentPost" :key="index">
+        </div>
+        <div v-for="tag,index of tags" :key="index">
+        </div>
     </div>
 </template>
 
---
+++ src/store/footer.js
@@ -0,0 +1,20 @@
+
+import { reactive } from "vue"
+export const topRatedProducts = reactive([
+    {
+
+    }
+])
+
+export const recentPost = reactive({
+    imgs: ['', '', ''],
+    products: [ '', '', '', '', '', '',]
+})
+
+export const tags = reactive([
+    'tag1', 'tag2'
+])
+
+
+
+
