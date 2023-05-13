5dc33fa0b60b0003e4b394d9a712e2bc5f0a7fc3
giovannilampis <giolampis@hotmail.com>
2023-05-13 21:20:00 +0200

    add SocialComp.vue & social.js to project

---
+++ src/components/SocialComp.vue
@@ -0,0 +1,27 @@
+<script>
+
+import { social_icons } from '../store/social'
+export default {
+    name: 'SocialComp',
+    data() {
+        return {
+            social_icons
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div>
+        <a v-for="(icon, index) in social_icons" :key="index" href="#">{{ icon }}</a>
+    </div>
+</template>
+
+<style>
+
+@use '../style/partials/variables' as *;
+
+@use '../style/partials/mixins' as *;
+
+</style>
---
+++ src/store/social.js
@@ -0,0 +1,8 @@
+import { reactive, ref } from "vue"
+
+export const social_icons = reactive([
+    '<i class="fa-brands fa-facebook-f"></i>',
+    '<i class="fa-brands fa-twitter"></i>',
+    '<i class="fa-brands fa-instagram"></i>',
+    '<i class="fa-brands fa-youtube"></i>'
+])
