ecd249f291b0505dde1e60c2d1a7c5d3542b80c8
giovannilampis <giolampis@hotmail.com>
2023-05-13 10:55:29 +0200

    add brandLogos.js to store

---
+++ src/store/brandLogos.js
@@ -0,0 +1,11 @@
+import { reactive } from "vue"
+
+export const brandLogos = reactive(
+    [
+        'b_logotype_6.png',
+        'b_logotype_7.png',
+        'b_logotype_1.png',
+        'b_logotype_2.png',
+        'b_logotype_3.png'
+    ]
+)
