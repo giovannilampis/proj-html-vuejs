666039ce384ba33b886b45a19a8a923c3b90dc40
giovannilampis <giolampis@hotmail.com>
2023-05-13 10:26:19 +0200

    add content to blogArticles in blogArticles.js

--- src/store/blogArticles.js
+++ src/store/blogArticles.js
@@ -2,12 +2,27 @@
 import { reactive } from "vue"
 export const blogArticles = reactive([
     {
-        img:'92124892-54cc2e00-edcc-11ea-8994-f15925e53b1e-400x300',
-        title: 'Summer collection',
-        description: '',
+        img:'post_img_12-700x441.jpg',
+        title: 'Spring collection',
+        description: 'Mario ipsum RGB mushroom 1-up. Invincible Luigi NES Mushroom Kingdom. ',
         data: 'September 9th,2015',
-        commenti: 1
+        comments: []
     },
+    {
+        img:'post_img_11-700x441.jpg',
+        title: 'Accessories collection',
+        description: 'Buzzy beetle koopa troopa brick 1985 lakitu. Bullet bill A-button bullet bill Mario Bros',
+        data: 'September 9th,2015',
+        comments: []
+    },
+    {
+        img:'post_img_10-700x441.jpg',
+        title: 'Art photography',
+        description: 'Cloud blooper 8-bit coin. Piranha plant swim bottomless pit bullet bill red shell fireball NES',
+        data: 'September 9th,2015',
+        comment: []
+    }
+    
     
 ])
 
