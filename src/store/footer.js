0ed6172c2665ae38311979e6cc787cfb14a7c204
giovannilampis <giolampis@hotmail.com>
2023-05-13 10:43:58 +0200

    sadd content to variables in footer.js

--- src/store/footer.js
+++ src/store/footer.js
@@ -1,18 +1,41 @@
 
 import { reactive } from "vue"
+
 export const topRatedProducts = reactive([
     {
-
+        img: 'leather_gloves-120x156.jpg',
+        name: 'Leather Gloves',
+        price: 45,
+        sale_price: null
+    },
+    {
+        img: 'black_elegant_leather_jacket-120x156.jpg',
+        name: 'Black Leather Jacket',
+        price: 235,
+        sale_price: 200
+    },
+    {
+        img: 'spring_printed_dress-120x156.jpg',
+        name: 'Printed Dress',
+        price: 47,
+        sale_price: null
     }
 ])
 
-export const recentPost = reactive({
-    imgs: ['', '', ''],
-    products: [ '', '', '', '', '', '',]
-})
+export const recentPost = reactive(
+    [
+        'Koopa troopa super mushroom buzzy',
+        'beetle Mario B-button run life, plumber 1-up',
+        'Super mushroom cheep cheep castle koopa paratroopa',
+        'Blooper coin jump brick super mushroom',
+        '1985 cloud slide spiny cloud, invincible Mario',
+        'coin Mario Bros fire bar, green shell plumber'
+    ]
+)
 
 export const tags = reactive([
-    'tag1', 'tag2'
+    'black', 'boots', 'brown', 'casual', 'D & G', 'fabric', 'flowers', 'grey', 'hat', 'hipster', 'lines',
+    'multi-purpose', 'New York', 'Outdoors', 'red', 'responsive', 'summer', 'sweater', 'Travel', 'Warm', 'White', 'winter'
 ])
 
 
