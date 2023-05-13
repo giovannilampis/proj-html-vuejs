87ef0034713bd022460de75895159bb566715dea
giovannilampis <giolampis@hotmail.com>
2023-05-13 12:20:18 +0200

    change images in blogArticles.js

--- src/components/BlogArticlesComp.vue
+++ src/components/BlogArticlesComp.vue
@@ -12,14 +12,23 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div v-for="article,index of blogArticles" :key="index">
-            {{  article }}
+    <div class="container-fluid row">
+        <div v-for="article,index of blogArticles" :key="index" class="col-4">
+            <div class="image-container">
+                <img :src="'/images/' + article.img" alt="article.title ">
+            </div>
+            <p>{{ article.title }}</p>
+            <span>{{ article.date }}</span>
+            <span>| Comments</span>
+            <!-- <span>{{ article.comments.length }}</span> -->
+            <p>{{ article.description }}</p>
         </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+    .image-container {
+        width: 100%;
+    }
 </style>
--- src/components/CollectionCard.vue
+++ src/components/CollectionCard.vue
@@ -13,8 +13,14 @@
 </script>
 
 <template>
-    <div class="col-4">
-       
+    
+    <div class="col-4" v-for=" season,index of seasonCollections" :key="index" style=" height: 500px; position: relative;">
+            <img style="object-fit: cover; position: absolute; z-index: -1; top: 0; left: 0; height: 100%; width: 100%;" 
+                :src="'/images/' + season.img" alt="{{ season.title }}">
+
+            <p>
+                {{ season.title }} 
+            </p>
     </div>
 
 </template>
