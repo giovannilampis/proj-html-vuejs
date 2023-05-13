f7cf29a126a7030b8f106abf935597ad1b877921
giovannilampis <giolampis@hotmail.com>
2023-05-13 16:17:58 +0200

    add img to jumbocomp

--- src/components/JumboComp.vue
+++ src/components/JumboComp.vue
@@ -7,12 +7,18 @@ export default {
 </script>
 
 <template>
-    <div>
-        
+    <div class="jumbo_background">
+        <img src="images/home1_slide_three_bg_2.jpg" alt="">
     </div>
 </template>
 
 
 <style lang="scss" scoped>
+
+.jumbo_background {
+    width: 100%;
+    height: 482px;
+    background-image: url('images/home1_slide_one_bg.jpg');
+}
 
 </style>
