5256f62a30ab31e3de1f63a088a3be28f751be10
giovannilampis <giolampis@hotmail.com>
2023-05-14 04:44:30 +0200

    add missing content to SummaryComp.vue

--- src/components/SummaryComp.vue
+++ src/components/SummaryComp.vue
@@ -1,8 +1,11 @@
 <script>
 
 import { subSummary }  from '../store/subSummary'
+import StarsComp from './StarsComp.vue'
+
 export default {
     name: 'SummaryComp',
+    components: {StarsComp},
     data() {
         return {
             subSummary
@@ -15,69 +18,77 @@ export default {
 <template>
     <div class="row py-5">
         <div class="col-3">
-            <div v-for="summary_featured,index of subSummary.featured" :key="index">
+            <p class="text-uppercase">featured</p>
+            <div v-for="summary_featured,index of subSummary.featured" :key="index" class="summary_element">
                 <div class="d-flex">
                     <div class="flex-1" style="flex: 1">
-                        {{ summary_featured.title }}
-                        Stelline
-                        <span v-if="summary_featured.sale_price != null">
-                            $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
-                        </span>
-                        <span v-else>
-                            ${{ summary_featured.price }}
-                        </span>
+                        <p>{{ summary_featured.title }}</p>
+                        <StarsComp/>
+                        <div>
+                            <span v-if="summary_featured.sale_price != null">
+                                $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
+                            </span>
+                            <span v-else>
+                                ${{ summary_featured.price }}
+                            </span>
+                        </div>
                     </div>
                     <img style="width: 90px;" :src="'/images/' + summary_featured.img" alt="">
                 </div>
             </div>
         </div>
         <div class="col-3">
-            <div v-for="summary_featured,index of subSummary.on_sale" :key="index">
+            <p class="text-uppercase">on sale</p>
+            <div v-for="summary_featured,index of subSummary.on_sale" :key="index" class="summary_element">
                 <div class="d-flex">
                     <div class="flex-1" style="flex: 1">
-                        {{ summary_featured.title }}
-                        Stelline
-                        <span v-if="summary_featured.sale_price != null">
-                            $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
-                        </span>
-                        <span v-else>
-                            ${{ summary_featured.price }}
-                        </span>
+                        <p>{{ summary_featured.title }}</p>
+                        <StarsComp/>
+                        <div>
+                            <span v-if="summary_featured.sale_price != null">
+                                $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
+                            </span>
+                            <span v-else>
+                                ${{ summary_featured.price }}
+                            </span>
+                        </div>
                     </div>
                     <img style="width: 90px;" :src="'/images/' + summary_featured.img" alt="">
                 </div>
             </div>
         </div>
         <div class="col-3">
-            <div v-for="summary_featured,index of subSummary.top_rated" :key="index">
+            <p class="text-uppercase">top rated</p>
+            <div v-for="summary_featured,index of subSummary.top_rated" :key="index" class="summary_element">
                 <div class="d-flex">
                     <div class="flex-1" style="flex: 1">
-                        {{ summary_featured.title }}
-                        Stelline
-                        <span v-if="summary_featured.sale_price != null">
-                            $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
-                        </span>
-                        <span v-else>
-                            ${{ summary_featured.price }}
-                        </span>
+                        <p>{{ summary_featured.title }}</p>
+                        <StarsComp/>
+                        <div>
+                            <span v-if="summary_featured.sale_price != null">
+                                $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
+                            </span>
+                            <span v-else>
+                                ${{ summary_featured.price }}
+                            </span>
+                        </div>
                     </div>
                     <img style="width: 90px;" :src="'/images/' + summary_featured.img" alt="">
                 </div>
             </div>
         </div>
         <div class="col-3">
-            <p class=""> Latest Reviews  </p>
-            <div v-for="summary_featured,index of subSummary.latest_reviews" :key="index">
+            <p class="text-uppercase">latest reviews</p>
+            <div v-for="summary_featured,index of subSummary.latest_reviews" :key="index" class="summary_element">
                 <div class="d-flex">
                     <div class="flex-1" style="flex: 1">
-                        {{ summary_featured.title }}
-                        Stelline
-                        <span v-if="summary_featured.sale_price != null">
-                            $<del>{{ summary_featured.price }}</del> ${{ summary_featured.sale_price }}
-                        </span>
-                        <span v-else>
-                            ${{ summary_featured.price }}
-                        </span>
+
+                        <p>{{ summary_featured.title }}</p>
+
+                        <StarsComp/>
+
+                        <p> by {{ summary_featured.author }}</p>
+
                     </div>
                     <img style="width: 80px;" :src="'/images/' + summary_featured.img" alt="">
                 </div>
@@ -92,5 +103,9 @@ export default {
 @use '../style/partials/variables' as *;
 
 @use '../style/partials/mixins' as *;
+
+.summary_element {
+    border-bottom: 1px solid $edward;
+}
 
 </style>
