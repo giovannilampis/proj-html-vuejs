3ba7c93306b9a533cc67dd4f24c278ad7bf7d2dc
giovannilampis <giolampis@hotmail.com>
2023-05-15 22:00:08 +0200

    add content to brandLogos.js

--- src/store/brandLogos.js
+++ src/store/brandLogos.js
@@ -6,6 +6,11 @@ export const brandLogos = reactive(
         'b_logotype_7.png',
         'b_logotype_1.png',
         'b_logotype_2.png',
+        'b_logotype_3.png',
+        'b_logotype_6.png',
+        'b_logotype_7.png',
+        'b_logotype_1.png',
+        'b_logotype_2.png',
         'b_logotype_3.png'
     ],
 )
