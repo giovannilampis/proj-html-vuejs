fa71af072075d2c69846fe80941a834f0f0dd2cd
giovannilampis <giolampis@hotmail.com>
2023-05-16 11:10:47 +0200

    change pictures in bestSellers.js

--- src/store/bestSeller.js
+++ src/store/bestSeller.js
@@ -1,16 +1,16 @@
 
 import { reactive } from "vue"
 export const bestSeller = reactive([
-   'spring_printed_dress-400x520.jpg',
-   'modern_love_tee-400x520.jpg',
-   'black_leather_jacket-400x520.jpg',
-   'black_elegant_leather_jacket-400x520.jpg',
-   'hipster_black_top-400x520.jpg',
-   'blue_leather_jacket-400x520.jpg',
-   'blue_jacket_and_white_stripe_tee-400x520.jpg',
-   'black_leather_suit-400x520.jpg',
-   'modern_black_leather_suit-400x520.jpg',
-   'modern_leather_boots-400x520.jpg'
+   'spring_printed_dress.jpg',
+   'modern_love_tee.jpg',
+   'black_leather_jacket.jpg',
+   'black_elegant_leather_jacket.jpg',
+   'hipster_black_top.jpg',
+   'blue_leather_jacket.jpg',
+   'blue_jacket_and_white_stripe_tee.jpg',
+   'black_leather_suit.jpg',
+   'modern_black_leather_suit.jpg',
+   'modern_leather_boots.jpg'
 ])
 
 
