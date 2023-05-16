54cc8362a0751598e948bbcce20162350c7249b4
giovannilampis <giolampis@hotmail.com>
2023-05-16 11:28:23 +0200

    change pics in new_arrivals.js 4 better quality

--- src/store/new_arrivals.js
+++ src/store/new_arrivals.js
@@ -1,17 +1,17 @@
 import { reactive } from "vue"
 
 export const new_arrivals = reactive([
-    'spring_printed_dress-231x300.jpg',
-    'blue_leather_jacket-231x300.jpg',
-    'black_leather_jacket-231x300.jpg',
-    'black_elegant_leather_jacket-231x300.jpg',
-    'hipster_black_top-231x300.jpg',
-    'leather_gloves-231x300.jpg',
-    'spring_printed_dress-231x300.jpg',
-    'blue_leather_jacket-231x300.jpg',
-    'black_leather_jacket-231x300.jpg',
-    'black_elegant_leather_jacket-231x300.jpg',
-    'hipster_black_top-231x300.jpg',
-    'leather_gloves-231x300.jpg',
+    'spring_printed_dress-400x520.jpg',
+    'blue_leather_jacket-400x520.jpg',
+    'black_leather_jacket-400x520.jpg',
+    'blue_leather_jacket-400x520.jpg',
+    'brown_dress_shoes-400x520.jpg',
+    'casual_leather_belts-400x520.jpg',
+    'hipster_black_top-400x520.jpg',
+    'leather_gloves-400x520.jpg',
+    'modern_black_leather_suit-400x520.jpg',
+    'modern_leather_boots-400x520.jpg',
+    'modern_love_tee-400x520.jpg',
+    'spring_printed_dress-400x520.jpg',
  ])
  
