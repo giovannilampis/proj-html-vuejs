bbf764fd3c1a0fe232276a5f7240568e10edecf1
giovannilampis <giolampis@hotmail.com>
2023-05-16 09:30:37 +0200

    add style to BlogArticlesComp title

--- src/components/BlogArticlesComp.vue
+++ src/components/BlogArticlesComp.vue
@@ -14,8 +14,15 @@ export default {
 <template>
     <section>
         <div class="container">
-            <h2 class="text-capitalize text-center fw-bold">from our blog</h2>
+            
+            <div class="title-container">
+                <div class="line line-left"></div>
+                <h2>from our blog</h2>
+                <div class="line line-right"></div>
+            </div>
+
             <p class="text-center pb-5">The latest Classic Shop news</p>
+
             <div class="row align-items-center justify-content-center">
                 <div v-for="article,index of blogArticles" :key="index" class="col-4">
                     <div class="image-container">
@@ -47,8 +54,28 @@ export default {
         height: 650px;
         padding-top: 5rem;
         border-bottom: 1px solid $edward;
-        
     }
+
+.title-container {
+  display: flex;
+  align-items: center;
+}
+
+h2 {
+    @include title;
+}
+
+.line {
+    @include line;
+}
+
+.line-left {
+  margin-right: 0;
+}
+
+.line-right {
+  margin-left: 0;
+}
 
     img {
         padding-bottom: 1rem;
