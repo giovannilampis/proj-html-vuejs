0f209aa926063b6d94b9b15dd76dce7406f29a32
giovannilampis <giolampis@hotmail.com>
2023-05-16 11:04:05 +0200

    change bestSeller.js content

--- src/store/bestSeller.js
+++ src/store/bestSeller.js
@@ -1,16 +1,16 @@
 
 import { reactive } from "vue"
 export const bestSeller = reactive([
-   'spring_printed_dress-200x260.jpg',
-   'modern_love_tee-200x260.jpg',
-   'black_leather_jacket-200x260.jpg',
-   'black_elegant_leather_jacket-200x260.jpg',
-   'hipster_black_top-200x260.jpg',
-   'spring_printed_dress-200x260.jpg',
-   'modern_love_tee-200x260.jpg',
-   'black_leather_jacket-200x260.jpg',
-   'black_elegant_leather_jacket-200x260.jpg',
-   'hipster_black_top-200x260.jpg'
+   'spring_printed_dress-400x520.jpg',
+   'modern_love_tee-400x520.jpg',
+   'black_leather_jacket-400x520.jpg',
+   'black_elegant_leather_jacket-400x520.jpg',
+   'hipster_black_top-400x520.jpg',
+   'blue_leather_jacket-400x520.jpg',
+   'blue_jacket_and_white_stripe_tee-400x520.jpg',
+   'black_leather_suit-400x520.jpg',
+   'modern_black_leather_suit-400x520.jpg',
+   'modern_leather_boots-400x520.jpg'
 ])
 
 
